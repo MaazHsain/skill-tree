28987249e39f6751d3e2f8788f7161834b1e5445
MaazHsain <maazhussain002@gmail.com>
2025-03-10 12:45:45 +0800

    Application server 5000

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__) # Needed so that Flask knows where to look for resources such as templates and static files
 CORS(app)
@@ -10,6 +11,17 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+dist_folder = os.path.join(os.getcwd(),"..","frontend","dist")
+
+# to serve static files from the 'dist' folder
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+  if not filename:
+    filename = "index.html"
+  return send_from_directory(dist_folder,filename)
+
+# api routes
 import routes
 
 with app.app_context():
